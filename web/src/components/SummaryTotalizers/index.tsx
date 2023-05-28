355c21551e60505a24ace904b01a19eeeb83c7da
BiaCarvalhoCavalieri <bia3003carvalho@gmail.com>
2023-05-28 18:36:53 -0300

    adding stylization to SummaryTotalizers component

--- web/src/components/SummaryTotalizers/index.tsx
+++ web/src/components/SummaryTotalizers/index.tsx
@@ -8,29 +8,28 @@ export const SummaryTotalizers = () => {
             <div className='summaryTotalizers__price--container'>
                 <div className='summaryTotalizers__price--description content-flex flex-column'>
                     <div className='summaryTotalizers__description--row content-flex justify-between'>
-                        <span>Subtotal</span>
-                        <span>R$ 10.986,90</span>
+                        <span className='summaryTotalizers__title'>Subtotal</span>
+                        <span className='summatyTotalizers__value'>R$ 10.986,90</span>
                     </div>
                     <div className='summaryTotalizers__description--row content-flex justify-between'>
-                        <span>Desconto</span>
-                        <span>R$ -2.069,70</span>
+                        <span className='summaryTotalizers__title'>Desconto</span>
+                        <span className='summatyTotalizers__value--negative'>R$ -2.069,70</span>
                     </div>
                     <div className='summaryTotalizers__description--row content-flex justify-between'>
-                        <span>Entrega</span>
-                        <span>R$ 36,90</span>
+                        <span className='summaryTotalizers__title'>Entrega</span>
+                        <span className='summatyTotalizers__value'>R$ 36,90</span>
                     </div>
                 </div>
                 <div className='summaryTotalizers__price--total content-flex flex-column'>
                     <div className='summaryTotalizers__description--row content-flex justify-between'>
-
-                        <span>Total</span>
-                        <span>R$8.876,50</span>
+                        <span className='summaryTotalizers__title'>Total</span>
+                        <span className='summatyTotalizers__total--value'>R$8.876,50</span>
                     </div>
-                    <p className='summaryTotalizers__installments--row content-flex '>
-                        em até
-                        <span>10x</span>
+                    <p className='summaryTotalizers__installments--row'>
+                        em até 
+                        <strong> 10x </strong>
                         de
-                        <span>R$ 887,65</span>
+                        <strong> R$ 887,65</strong>
                     </p>
                 </div>
             </div>
